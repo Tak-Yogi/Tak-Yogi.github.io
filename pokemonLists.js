7d78e92a53f1cd1461dab21c2a9b85113e1bf879
Tak-Yogi <125669703+Tak-Yogi@users.noreply.github.com>
2023-03-28 08:06:34 +0900

    Add files via upload

--- pokemonLists.js
+++ pokemonLists.js
@@ -1,32 +1,38 @@
 'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
 
+// document.querySelector('#pokemon-list')によって、
+// HTML内のidがpokemon-listである要素を取得
 const pokemonList = document.querySelector('#pokemon-list');
 
+// 引数としてポケモンのオブジェクトを受け取り、そのポケモンのカードを作成
+// カードはdiv要素で、その中にポケモンの番号('h2')、名前('h3')、タイプ('p')が表示
 function createPokemonCard(pokemon) {
     const card = document.createElement('div');
     card.classList.add('card');
 
-    const pokemonHeader = document.createElement('div');
-    pokemonHeader.className = 'pokemon-header';
-    card.appendChild(pokemonHeader);
-
+    // 番号を表示
     const number = document.createElement('h2');
     number.textContent = pokemon.Number;
     card.appendChild(number);
 
+    // 名前を表示
     const name = document.createElement('h3');
     name.textContent = pokemon.Name;
     card.appendChild(name);
 
+    // 画像を表示
     const pokemonImage = document.createElement('img');
     pokemonImage.src = `images/${pokemon.Number}.png`;
     pokemonImage.alt = pokemon.Name;
     card.appendChild(pokemonImage);
 
+    // タイプを表示
     const types = document.createElement('p');
     types.textContent = pokemon.Types.join(', ');
     card.appendChild(types);
 
+    // カードをクリックした際、showPokemonDetails関数を呼び出す
     card.addEventListener('click', () => {
         showPokemonDetails(pokemon);
     });
@@ -34,12 +40,16 @@ function createPokemonCard(pokemon) {
     return card;
 }
 
+// 名前のリストを取得し、それらのポケモンを取得し、
+// それぞれのカードを作成し、画面に表示
 function showPokemonList() {
-    getNames().forEach(name => {
+    const names = getNames();
+    for (const name of names) {
         const pokemon = getPokemonByName(name);
         const card = createPokemonCard(pokemon);
         pokemonList.appendChild(card);
-    });
+    }
 }
 
+// 最初に全てのポケモンを表示
 showPokemonList(allPokemon);
